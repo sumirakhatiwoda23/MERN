068f9ddd71731b8c9d32b8e929e4308eeabb3246
sumira2222 <sumira2222@example.com>
2025-12-13 21:36:13 +0545

    css create push

---
+++ html/sk.js
@@ -0,0 +1,58 @@
+/*
+primitive types
+string
+number
+boolean
+null
+undefined
+----------------------------
+reference types
+array
+object
+function
+*/
+
+
+
+// const persons = ['ram', 'shyam', 'hari'];
+// forEach, map, find, filter, reduce
+
+// persons[0] = 'rita';
+// persons.push('lio', 'mio');
+//persons.pop();
+//persons.shift();
+//persons.unshift('rio')
+
+//console.log(persons.at(0));
+//console.log(persons.concat('t', 's', 'l'));
+//console.log(persons.copyWithin(2, 1))
+//console.log(persons.fill(90, 1));
+//console.log(persons.includes('ram'));
+//console.log(persons.indexOf('ram'));
+//console.log(persons.reverse());
+//console.log(persons.length);
+// console.log(persons.slice(1, 3));
+// persons.splice(2, 1);
+// console.log(persons);
+
+
+
+
+
+// loops
+const persons=['ram','shyam','hari'];
+// while loop
+
+// let i=0;
+// while(i<20){
+//     console.log(hello ${i});
+//     i++;
+// }
+
+let i=0;
+sum=0;
+while(i<=10){
+    sum=i++;
+    sum++;
+}
+console.log(sum)
