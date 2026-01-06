0d1c66c5c10e49a9e61350e4ee9958ef1db0950a
sumira2222 <sumira2222@example.com>
2026-01-06 07:05:25 +0545

    changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold text-red-400'>hello jee</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa, vitae.
+      </p>
+    </div>
+  )
+}
