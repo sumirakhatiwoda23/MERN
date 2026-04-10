e74529a0ab5ff326030179ab69cda3d8de0a96a5
sumira2222 <sumira2222@example.com>
2026-04-10 22:52:50 +0545

    update message

--- src/todos/TodoAddFrom.jsx
+++ src/todos/TodoAddFrom.jsx
@@ -3,6 +3,31 @@ import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader,
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label"
 import { Formik } from "formik";
+import * as Yup from "yup";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import {
+  Select,
+  SelectLabel,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+import { Switch } from "@/components/ui/switch"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Field, FieldContent, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field"
+
+
+
+const todoSchema = Yup.object({
+  email: Yup.string().email().required(),
+  gender: Yup.string().required(),
+  country: Yup.string().required(),
+  message: Yup.string().min(10).max(200).required(),
+  airplanemode: Yup.boolean().required()
+});
 
 
 export default function TodoAddFrom() {
@@ -20,34 +45,143 @@ export default function TodoAddFrom() {
 
         <Formik
           initialValues={{
-            email: ''
-
+            email: '',
+            gender: '',
+            country: '',
+            message: '',
+            airplanemode: false,
+            habits:[]
           }}
 
           onSubmit={(val) => {
             console.log(val);
 
           }}
+          validationSchema={todoSchema}
         >
 
 
           {({ handleChange, handleSubmit, values, errors, touched, setFieldValue }) => (
             <form onSubmit={handleSubmit}>
               <div className="flex flex-col gap-6">
+
                 <div className="grid gap-2">
                   <Label htmlFor="email">Email</Label>
                   <Input
                     id="email"
                     name="email"
                     onChange={handleChange}
-                    type="email"
+                    value={values.email}
                     placeholder="m@example.com"
 
                   />
+                  {errors.email && touched.email && <div className="text-red-500">{errors.email}</div>}
                 </div>
 
+                <div className="grid gap-2">
+                  <Label htmlFor="gender">Select Your Gender</Label>
+                  <RadioGroup
+                    name="gender"
+                    onChange={handleChange}
+                    className="w-fit mt-2">
+
+                    <div className="flex items-center gap-3">
+                      <RadioGroupItem value="male" id="r2" />
+                      <Label htmlFor="r2">Male</Label>
+                    </div>
+                    <div className="flex items-center gap-3">
+                      <RadioGroupItem value="female" id="r3" />
+                      <Label htmlFor="r3">Female</Label>
+                    </div>
+                  </RadioGroup>
+
+                  {errors.gender && touched.gender && <div className="text-red-500">{errors.gender}</div>}
+                </div>
+
+
+                <div className="grid gap-2">
+                  <Label htmlFor="country">Select Your Country</Label>
+                  <Select
+                    name="country"
+                    onValueChange={(e) => setFieldValue("country", e)}
+                  >
+                    <SelectTrigger className="w-full max-w-48">
+                      <SelectValue placeholder="Select country" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Country</SelectLabel>
+                        <SelectItem value="nepal">Nepal</SelectItem>
+                        <SelectItem value="india">India</SelectItem>
+                        <SelectItem value="china">China</SelectItem>
+
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+
+                  {errors.country && touched.country && <div className="text-red-500">{errors.country}</div>}
+
+
+
+                </div>
+
+                <div>
+                  <Textarea
+                    name="message"
+                    onChange={handleChange}
+                    value={values.message}
+                    placeholder="Type your message here." />
+                  {errors.message && touched.message && <div className="text-red-500">{errors.message}</div>}
+                </div>
+
+                <div className="flex items-center space-x-2">
+                  <Switch
+                    name="airplanemode"
+                    onCheckedChange={(e) => setFieldValue("airplanemode", e)}
+                    id="airplane-mode" />
+                  <Label htmlFor="airplane-mode">Airplane Mode</Label>
+                </div>
+
+
+
+
+                <div>
+
+                  <Label htmlFor="habits">Select Your Habits</Label>
+
+                  <FieldGroup
+
+                    className="max-w-sm mt-4">
+                    <Field orientation="horizontal">
+                      <Checkbox
+                        onCheckedChange={(e) => {
+                       setFieldValue('habits',e);
+                        }}
+                        id="habits"
+                        value="Dance" name="habits" />
+                      <Label htmlFor="habits">Dance</Label>
+                    </Field>
+
+                    <Field orientation="horizontal">
+                      <Checkbox id="terms-checkbox" 
+                      value="Sing"
+                      name="habits" />
+                      <Label htmlFor="terms-checkbox">Sing</Label>
+                    </Field>
+
+
+                  </FieldGroup>
+
+                </div>
+
+
+
+
+
+
+
               </div>
-              <Button type="submit" className=" cursor-pointer mt-7 w-full">
+              <Button type="submit" className=" mt-7 w-full">
                 Submit
               </Button>
             </form>
